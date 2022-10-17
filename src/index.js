6d8baefc51c580285027aa7380ceffb724b11803
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2022-10-17 16:10:26 +0400

    If-else code

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// components
+export {If, Else} from './components/if-else'
+
+// hooks
+export {useThis} from './hooks/useThis'
+export {usePrevious} from './hooks/usePrevious'
+export {useConstructor} from './hooks/useConstructor'
