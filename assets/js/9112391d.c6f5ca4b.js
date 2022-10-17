88b3bfafe02c4216210754f8aa868d04fcf0e4e7
ruben-arushanyan <ruben.arushanyan24@gmail.com>
2022-10-17 13:04:24 +0000

    deploy: b97a513fab2d6cfbfeb33d8a79c7ef8b0a709038

---
+++ assets/js/9112391d.c6f5ca4b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreact_comfort_website=self.webpackChunkreact_comfort_website||[]).push([[804],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ assets/js/ba4ce4c8.3ebf32c2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreact_comfort_website=self.webpackChunkreact_comfort_website||[]).push([[345],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
